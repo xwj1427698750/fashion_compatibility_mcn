9106e09e739c56c1ee871a1e051a0778cf6af671
xwj1427698750 <17352959682@163.com>
2021-10-31 12:26:46 +0800

    修改评测文件

--- mult_module_generator/evaluate.py
+++ mult_module_generator/evaluate.py
@@ -47,23 +47,53 @@ model.eval()
 test_num = 10
 auc_epochs = []
 fitb_epochs = []
+log_sigmoid = nn.LogSigmoid()
 for epoch in range(test_num):
     total_loss = AverageMeter()
     clf_losses = AverageMeter()
+    bpr_losses = AverageMeter()
+    clf_diff_acc = AverageMeter()
     outputs = []
     targets = []
     for batch_num, batch in enumerate(test_loader, 1):
         print("\r#{}/{}".format(batch_num, len(test_loader)), end="", flush=True)
-        lengths, images, names, set_id, labels, is_compat = batch
+        lengths, images, names, set_id, labels = batch
         images = images.to(device)
-        target = is_compat.float().to(device)
         with torch.no_grad():
-            output, _ = model.conpute_compatible_score(images)
-            output = output.squeeze(dim=1)
-            loss = criterion(output, target)
-        total_loss.update(loss.item(), images.shape[0])
+            pos_out, neg_out, diff, _ = model.conpute_compatible_score(images)
+            # clf_loss
+        batch_size, _, _, _, img_size = images.shape  # [16, 5, 3, 224, 224]
+        pos_target = torch.ones(batch_size)
+        pos_out = pos_out.squeeze(dim=1)
+        neg_target = torch.zeros(batch_size)
+        neg_out = neg_out.squeeze(dim=1)
+        output = torch.cat((pos_out, neg_out))
+        target = torch.cat((pos_target, neg_target)).to(device)
+        clf_loss = criterion(output, target)
+        # bpr_loss
+        bpr_param = 1
+        bpr_loss = bpr_param * torch.sum(-log_sigmoid(diff))
+        # diff score
+        diff = diff.squeeze(dim=1)
+        diff_sum = torch.sum((diff > 0).float())
+        clf_diff_acc.update(diff_sum.item(), images.shape[0])
+        clf_losses.update(clf_loss.item(), images.shape[0])
+        bpr_losses.update(bpr_loss.item(), images.shape[0])
         outputs.append(output)
         targets.append(target)
+    outputs = torch.cat(outputs).cpu().data.numpy()
+    targets = torch.cat(targets).cpu().data.numpy()
+    logging.info("clf_diff_acc@0.5: {:.4f}".format(clf_diff_acc.avg))
+    auc = metrics.roc_auc_score(targets, outputs)
+    logging.info("AUC: {:.4f}".format(auc))
+    predicts = np.where(outputs > 0.5, 1, 0)
+    accuracy = metrics.accuracy_score(predicts, targets)
+    logging.info("Accuracy@0.5: {:.4f}".format(accuracy))
+
+    positive_loss = -np.log(outputs[targets == 1]).mean()
+    logging.info("Positive loss: {:.4f}".format(positive_loss))
+    positive_acc = sum(outputs[targets == 1] > 0.5) / len(outputs)
+    logging.info("Positive accuracy: {:.4f}".format(positive_acc))
     print()
     print("test:{} Test Loss: {:.4f}".format(epoch + 1, total_loss.avg))
     outputs = torch.cat(outputs).cpu().data.numpy()
@@ -78,15 +108,16 @@ for epoch in range(test_num):
         print("\r#{}/{}".format(i, len(test_dataset)), end="", flush=True)
         items, labels, question_part, question_id, options, option_labels = test_dataset.get_fitb_quesiton(i)
         question_part_id = {"upper": 0, "bottom": 1, "bag": 2, "shoe": 3}.get(question_part)
-        images = [items]
+        images = []
         for option in options:
             new_outfit = items.clone()
-            new_outfit[question_part_id] = option
+            new_outfit = torch.cat((new_outfit, option), 1)
             images.append(new_outfit)
         images = torch.stack(images).to(device)
-        names = []  # 未使用
-        output, _ = model.conpute_compatible_score(images)
-        if output.argmax().item() == 0:
+        pos_out, neg_out, diff, _ = model.conpute_compatible_score(images)
+        diff = diff.squeeze(dim=1)
+        diff = torch.sum((diff > 0).float())
+        if int(diff) == len(options):
             is_correct.append(True)
         else:
             is_correct.append(False)
